db0f195838170cb4d30abc7bf5e2c3b66659cbb1
Angel M. Adames <angelmadames@gmail.com>
2024-01-29 21:14:27 -0400

    chore: Add tests to current-project command

--- src/commands/config/current-project.ts
+++ src/commands/config/current-project.ts
@@ -1,20 +1,24 @@
-import * as fs from 'node:fs';
 import { Command } from 'commander';
+import * as fs from 'node:fs';
 import cliConfig from '../../config/cli';
 
-const CurrentProjectCommand = new Command();
+export const currentProjectCommand = () => {
+  const command = new Command();
+  command
+    .name('current-project')
+    .summary('Manage DEMS current project state')
+    .description('Manage the current project state of DEMS')
+    .option('-s, --set <project>', 'Set current project to a new value')
+    .action((options) => {
+      if (options.set) {
+        fs.writeFileSync(cliConfig.currentProjectFile, options.set);
+        console.log(`Current project set to: ${options.set}`);
+      } else {
+        console.log(`Current project is: ${cliConfig.currentProject}`);
+      }
+    });
 
-CurrentProjectCommand.name('current-project')
-  .summary('Manage DEMS current project state')
-  .description('Manage the current project state of DEMS')
-  .option('-s, --set <project>', 'Set current project to a new value')
-  .action((options) => {
-    if (options.set) {
-      fs.writeFileSync(cliConfig.currentProjectFile, options.set);
-      console.log(`Current project set to: ${options.set}`);
-    } else {
-      console.log(`Current project is: ${cliConfig.currentProject}`);
-    }
-  });
+  return command;
+};
 
-export default CurrentProjectCommand;
+export default currentProjectCommand();
--- test/config.current-project.test.ts
+++ test/config.current-project.test.ts
@@ -1,12 +1,20 @@
 import { expect, test, describe } from 'bun:test';
-import CurrentProjectCommand from '../src/commands/config/current-project';
+import { currentProjectCommand } from '../src/commands/config/current-project';
+import { spawnSync } from 'bun';
 
 describe('Test config current-project', () => {
   test('Current project is set by --set flag', () => {
-    const command = CurrentProjectCommand;
+    const command = currentProjectCommand();
     const current = 'testProject';
     expect(command.setOptionValue('set', current).getOptionValue('set')).toBe(
       current,
     );
+  });
+
+  test('Cuurent project is set by environment variable', async () => {
+    const result = spawnSync(['bun', './cli.ts', 'config', 'current-project'], {
+      env: { ...process.env, DEMS_CURRENT_PROJECT: 'dev' },
+    });
+    expect(result.stdout.toString()).toContain('dev');
   });
 });
