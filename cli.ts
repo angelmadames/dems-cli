23f3d1d1cfec1f46ed02a7db1ebe53cec633872e
Angel M. Adames <angelmadames@gmail.com>
2024-04-01 19:26:12 -0400

    docs: Update summary and description of CLI entrypoint

--- cli.ts
+++ cli.ts
@@ -8,13 +8,22 @@ import { environmentCommand } from './src/commands/environment';
 import { composeCommand } from './src/commands/compose';
 import { dependenciesCommand } from './src/commands/dependencies';
 import { gitCommand } from './src/commands/git';
+import { noIndent } from './src/utils/string';
 
 const cli = new Command();
 
 cli
   .name('dems-cli')
-  .description('DEMS (Development Environment Management System) CLI')
   .version('0.0.1')
+  .summary('DEMS (Development Environment Management System) CLI')
+  .description(
+    noIndent(`
+      DEMS is an easy-to-use automation tool to provision development
+      environments for compatible application repositories. It uses
+      Docker, Docker Compose, and the power of Bun to automate the
+      initialization of a project and all of its respositories.
+    `)
+  )
   .addCommand(configCommand())
   .addCommand(setupCommand())
   .addCommand(gitCommand())
